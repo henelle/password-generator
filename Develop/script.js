1a121f3b6a7adb678229069655d1254c49cdd8a9
Hien Nguyen <FyreSchetz@yahoo.com>
2021-08-28 15:29:44 -0500

    add first part of functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
 
+// Function for upper case letters only
+function upperCase(length) {
+  var bigLetters = "";
+  var upperCaseSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var upperCaseLength = upperCaseSymbols.length;
+  for ( var i = 0; i < length; i++ ) {
+    bigLetters += upperCaseSymbols.charAt(Math.floor(Math.random() * upperCaseLength));
+  }
+  return bigLetters;
+}
+
+// Function for lower case letters only
+function lowerCase(length) {
+  var smallLetters = "";
+  var lowerCaseSymbols = "abcdefghijklmnopqrstuxwxyz";
+  var lowerCaseLength = lowerCaseSymbols.length;
+  for ( var i = 0; i < length; i++ ) {
+    smallLetters += lowerCaseSymbols.charAt(Math.floor(Math.random() * lowerCaseLength));
+  }
+  return smallLetters;
+}
+
+// Function for special characters only
+function special(length) {
+  var specialLetters = "";
+  var specialSymbols =  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var specialLength = specialSymbols.length;
+  for ( var i = 0; i < length; i++ ) {
+    specialLetters += specialSymbols.charAt(Math.floor(Math.random() * specialLength));
+  }
+  return specialLetters;
+}
+
 // Function to generate a random password based on which parameters is selected
 var generatePassword = function() {
   
